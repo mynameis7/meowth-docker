fa2d247b4854f3a27d13d93adde745bf844f2ccd
Andrew Rather <andrew.rather5@gmail.com>
2023-10-05 00:00:04 -0400

    fix postgres hostname setting

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ cp /code/meowth/config_template.py /code/meowth/config.py
 sed -i -e "s/your_token_here/$BOT_TOKEN/g" /code/meowth/config.py
 sed -i -e "s/# 'username' : 'meowth'/'username' : '$PG_USERNAME'/g" /code/meowth/config.py
 sed -i -e "s/# 'database' : 'meowth'/'database' : '$PG_DATABASE'/g" /code/meowth/config.py
-sed -i -e "s/# 'hostname' : 'localhost'/'database' : '$PG_HOSTNAME'/g" /code/meowth/config.py
+sed -i -e "s/# 'hostname' : 'localhost'/'hostname' : '$PG_HOSTNAME'/g" /code/meowth/config.py
 sed -i -e "s/'password' : 'password'/'password' : '$PG_PASSWORD'/g" /code/meowth/config.py
 cd /code/
 python setup.py install
